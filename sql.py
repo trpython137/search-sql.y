1ce170c5d2cbf3acf16cd8a4dec5302db2dd8879
trpython137 <118639796+trpython137@users.noreply.github.com>
2023-03-18 16:36:35 +0100

    Update and rename README.md to sql.py

---
+++ sql.py
@@ -0,0 +1,42 @@
+# search-sql.y
+from __future__ import absolute_import
+
+from __future__ import print_function
+
+import requests, sys, threading, time, os
+
+from random import randint
+
+from six.moves import input
+
+CheckVersion = str (sys.version)
+
+import re
+
+from datetime import datetime
+
+print ('''
+
+          \|_|/  Author: ANAS WOX
+
+          /. .\  Version: 1v
+
+         =\_Y_/= whatsapp 0619063649
+
+          {>o<}  Insta:h0k.eer 
+
+        \033[1;32m  _           _
+
+\033[1;36m
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+''')
+
+from googlesearch import search
+
+anas = 'index.php?id=5'
+
+for a in search (anas, num_results=40):
+
+      print (a)
